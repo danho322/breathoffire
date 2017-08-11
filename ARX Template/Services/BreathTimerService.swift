8fa83042aaedb3a1795820c8e9aa442c97f93b54
Daniel Ho <dho@gofundme.com>
2017-08-10 22:17:35 -0700

    Added breathing view

---
+++ ARX Template/Services/BreathTimerService.swift
@@ -0,0 +1,9 @@
+//
+//  BreathTimerService.swift
+//  ARX Template
+//
+//  Created by Daniel Ho on 8/8/17.
+//  Copyright © 2017 Daniel Ho. All rights reserved.
+//
+
+import Foundation
---
+++ ARX Template/UI/Views/BreathTimerView.swift
@@ -0,0 +1,21 @@
+//
+//  BreathTimerView.swift
+//  ARX Template
+//
+//  Created by Daniel Ho on 8/8/17.
+//  Copyright © 2017 Daniel Ho. All rights reserved.
+//
+
+import UIKit
+
+class BreathTimerView: XibView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
